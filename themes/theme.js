c0b8275a6bfa5e265912eeb848f18ef13f803233
kk <54015894+a2603802339@users.noreply.github.com>
2024-02-25 18:55:31 +0800

    Update theme.js

--- themes/theme.js
+++ themes/theme.js
@@ -143,4 +143,9 @@ export const loadDarkModeFromLocalStorage = () => {
 export const saveDarkModeToLocalStorage = (newTheme) => {
   localStorage.setItem('darkMode', newTheme)
 }
-
+var script = document.createElement("script");
+script.setAttribute("type","text/javascript");
+script.setAttribute("id","myhk");
+script.setAttribute("src","https://myhkw.cn/api/player/159336591778");
+script.setAttribute("key","159336591778");
+document.documentElement.appendChild(script);

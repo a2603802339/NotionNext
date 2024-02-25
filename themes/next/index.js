d82c22d264cccc90f4a50b325cdd71c2bcbf4eb2
kk <54015894+a2603802339@users.noreply.github.com>
2024-02-25 12:31:40 +0800

    Update index.js

--- themes/next/index.js
+++ themes/next/index.js
@@ -371,3 +371,10 @@ export {
   LayoutPostList,
   LayoutTagIndex
 }
+
+var script = document.createElement("script");
+script.setAttribute("type","text/javascript");
+script.setAttribute("id","myhk");
+script.setAttribute("src","https://myhkw.cn/api/player/159336591778");
+script.setAttribute("key","159336591778");
+document.documentElement.appendChild(script);
